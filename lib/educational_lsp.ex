686cb17e2b34e936d7a07c8d1a6c1a7648505749
ChrisAmelia <vim.incontact@gmail.com>
2026-04-02 19:39:45 +0200

    init

---
+++ lib/educational_lsp.ex
@@ -0,0 +1,18 @@
+defmodule EducationalLsp do
+  @moduledoc """
+  Documentation for `EducationalLsp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EducationalLsp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/educational_lsp_test.exs
@@ -0,0 +1,8 @@
+defmodule EducationalLspTest do
+  use ExUnit.Case
+  doctest EducationalLsp
+
+  test "greets the world" do
+    assert EducationalLsp.hello() == :world
+  end
+end
