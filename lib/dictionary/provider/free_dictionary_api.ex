49b1fa15c96519169d9553d23b2e98703da8f47d
ChrisAmelia <vim.incontact@gmail.com>
2026-04-15 14:20:49 +0200

    style: different format for phonetics/definitions

--- lib/dictionary/provider/free_dictionary_api.ex
+++ lib/dictionary/provider/free_dictionary_api.ex
@@ -12,18 +12,31 @@ defmodule Dictionary.Provider.FreeDictionaryAPI do
     {_, env} = Tesla.get(client, "https://api.dictionaryapi.dev/api/v2/entries/en/" <> word)
     data = Jason.decode!(env.body)
 
-    phonetic = get_in(data, [Access.at(0), "phonetic"])
-    definitions = get_in(data, [Access.at(0), "meanings", Access.at(0), "definitions"])
-
-    description =
-      definitions
-      |> Enum.with_index(1)
-      |> Enum.map_join("\n", fn {entry, index} -> "#{index}. #{entry["definition"]}" end)
+    phonetic = get_phonetics(data)
+    definitions = get_definitions(data)
 
     phonetic <>
       "\n\n" <>
-      description
+      definitions
   rescue
     _ -> "Couldn't find definition for '#{word}'"
+  end
+
+  defp get_phonetics(data) do
+    phonetics = get_in(data, [Access.at(0), "phonetics"])
+
+    phonetics
+    |> Enum.map(fn map -> map["text"] end)
+    |> Enum.filter(&(&1 != nil))
+    |> Enum.with_index(1)
+    |> Enum.map_join(", ", fn {entry, index} -> "(#{index}) #{entry}" end)
+  end
+
+  defp get_definitions(data) do
+    definitions = get_in(data, [Access.at(0), "meanings", Access.at(0), "definitions"])
+
+    definitions
+    |> Enum.with_index(1)
+    |> Enum.map_join("\n", fn {entry, _index} -> "1. #{entry["definition"]}" end)
   end
 end
